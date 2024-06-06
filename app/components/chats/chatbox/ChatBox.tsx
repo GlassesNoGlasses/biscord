dfac3bd7e1a362d6166c36cae64558ccaa4c18f8
Neil Wang <neilywang3@gmail.com>
2024-06-05 19:35:49 -0700

    finished text messaging input

---
+++ app/components/chats/chatbox/ChatBox.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import React from 'react'
+import { ChatBoxProps } from './ChatBoxProps'
+import { Message } from '@/app/interfaces/Message'
+
+export const ChatBox = ({
+  lastMessageID,
+  sender,
+  receiver,
+  callback
+}: ChatBoxProps) => {
+
+  // placeholder for incrementing message ids
+  let increment = 1;
+
+  // states
+  const [text, setText] = React.useState<string>('');
+
+  // handle enter key press
+  const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && text.length > 0) {
+      const newMessage: Message = {
+        id: lastMessageID + increment,
+        sender: sender,
+        receiver: receiver,
+        date: new Date(),
+        text: text
+      };
+
+      callback(newMessage);
+      setText('');
+    }
+  };
+
+  // handle text change
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  }
+
+  return (
+    <div className='flex w-full p-4 h-fit flex-row align-middle'>
+        <button className='flex bg-slate-700 hover:brightness-110 rounded h-full w-fit'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 rounded">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+            </svg>
+        </button>
+        <input
+        type='text'
+        placeholder={`Message @${receiver}`}
+        className='flex w-full h-fit bg-slate-600 text-white rounded-lg p-2'
+        onKeyDown={handleEnter}
+        value={text}
+        onChange={handleTextChange}/>
+    </div>
+  )
+}
+
--- app/components/nav-bar/NavBar.tsx
+++ app/components/nav-bar/NavBar.tsx
@@ -85,12 +85,21 @@ const NotLoggedIn = (): JSX.Element => {
 
 const LoggedIn = (LogOutCallBack: () => void): JSX.Element => {
     return (
-        <Link className='p-2 px-5 m-2 bg-red-500 hover:bg-red-600 active:bg-red-700
-			text-white font-bold rounded-lg shadow-md transition-colors duration-150 ease-in
-			text-base hover:brightness-110'
-			href='/signup' onClick={LogOutCallBack}>
-				Sign Out
-		</Link>
+        <> 
+            <Link className='p-2 px-5 m-2 bg-red-500 hover:bg-red-600 active:bg-red-700
+                text-white font-bold rounded-lg shadow-md transition-colors duration-150 ease-in
+                text-base hover:brightness-110'
+                href='/signup' onClick={LogOutCallBack}>
+                    Sign Out
+            </Link>
+            <Link className='h-fit w-fit'
+            href='/settings'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-12 rounded">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+            </Link>
+        </>
+
     )
 }
 
