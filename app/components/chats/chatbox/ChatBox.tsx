6e8d1daf4daab39beba3928bc0bfa96f5c554274
Neil Wang <neilywang3@gmail.com>
2024-06-07 14:14:45 -0700

    added file upload and styling

--- app/components/chats/chatbox/ChatBox.tsx
+++ app/components/chats/chatbox/ChatBox.tsx
@@ -39,12 +39,20 @@ export const ChatBox = ({
   }
 
   return (
-    <div className='flex w-full p-4 h-fit flex-row align-middle'>
-        <button className='flex bg-slate-700 hover:brightness-110 rounded h-full w-fit'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 rounded">
+    <div className='flex w-full p-4 h-fit flex-row align-middle justify-center gap-3'>
+        <input
+        type='file'
+        id='chatbox-file-input'
+        multiple={true}
+        className='invisible hidden'
+        />
+
+        <label htmlFor='chatbox-file-input' className='flex bg-slate-700 hover:brightness-110 rounded-lg border-2 border-gray-400 h-full w-fit'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className=" size-8">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
             </svg>
-        </button>
+        </label>
+        
         <input
         type='text'
         placeholder={`Message @${receiver}`}
